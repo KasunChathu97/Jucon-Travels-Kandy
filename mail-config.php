74c398490a82130755e6ebdedac6576e2ed180cc
KasunChathu97 <kasundeni1997@gmail.com>
2026-04-02 13:33:51 +0530

    add send email

--- mail-config.php
+++ mail-config.php
@@ -7,12 +7,12 @@
 // - The "from" address should normally match the authenticated SMTP account.
 
 return [
-    'to_email' => 'dmadhu2008@gmail.com',
+    'to_email' => 'jucontravels@gmail.com',
     'to_name' => 'JUCON TRAVELS Admin',
 
     // Sender identity shown in the email.
     // Put the email address of your SMTP account here.
-    'from_email' => 'dmadhu2008@gmail.com',
+    'from_email' => 'jucontravels@gmail.com',
     'from_name' => 'JUCON TRAVELS',
 
     // SMTP server settings
@@ -20,6 +20,6 @@ return [
     'smtp_port' => 587,
     'smtp_secure' => 'tls', // 'tls' or 'ssl'
     // For Gmail: use your full Gmail address and a Google App Password.
-    'smtp_username' => 'dmadhu2008@gmail.com',
-    'smtp_password' => 'slicdibxmrcwryao',
+    'smtp_username' => 'jucontravels@gmail.com',
+    'smtp_password' => 'jwupeyicisxunhdf',
 ];
